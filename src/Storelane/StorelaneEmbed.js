d4ed8eff2b8329b5d661a1c26176cb088f6f5b20
Gurdip Singh <gusingh@destm.com>
2025-11-26 17:11:53 +0530

    deleating Build

--- src/Storelane/StorelaneEmbed.js
+++ src/Storelane/StorelaneEmbed.js
@@ -1,6 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const StorylaneEmbed = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobileView = () => setIsMobile(window.innerWidth <= 768);
+    checkMobileView();
+    window.addEventListener("resize", checkMobileView);
+    return () => window.removeEventListener("resize", checkMobileView);
+  }, []);
+
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://js.storylane.io/js/v2/storylane.js";
@@ -14,56 +23,72 @@ const StorylaneEmbed = () => {
 
   return (
     <div style={{ position: "relative" }}>
-      <div
-        className="sl-embed"
-        style={{
-          position: "relative",
-          paddingBottom: "calc(65.64% + 25px)",
-          width: "100%",
-          height: 0,
-          transform: "scale(1)",
-        }}
-      >
-        <iframe
-          loading="lazy"
-          title="Storylane Embed"
-          class="sl-demo"
-          src="https://app.storylane.io/demo/rmtmua8ep9hv?embed=inline"
-          name="sl-embed"
-          allow="fullscreen"
-          allowfullscreen
+      {!isMobile ? (
+        <div
+          className="sl-embed"
           style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
+            position: "relative",
+            paddingBottom: "calc(65.64% + 25px)",
             width: "100%",
-            height: "100%",
-            border: "1px solid rgba(63,95,172,0.35)",
-            boxShadow: "0px 0px 18px rgba(26, 19, 72, 0.15)",
-            borderRadius: "10px",
-            boxSizing: "border-box",
+            height: 0,
+            transform: "scale(1)",
           }}
-        ></iframe>
-        {/* <iframe
-          loading="lazy"
-          class="sl-demo"
-          src="https://app.storylane.io/demo/rmtmua8ep9hv?embed=inline"
-          name="sl-embed"
-          allow="fullscreen"
-          allowfullscreen
+        >
+          <iframe
+            loading="lazy"
+            title="Storylane Embed"
+            class="sl-demo"
+            src="https://app.storylane.io/demo/rmtmua8ep9hv?embed=inline"
+            name="sl-embed"
+            allow="fullscreen"
+            allowfullscreen
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              border: "1px solid rgba(63,95,172,0.35)",
+              boxShadow: "0px 0px 18px rgba(26, 19, 72, 0.15)",
+              borderRadius: "10px",
+              boxSizing: "border-box",
+            }}
+          ></iframe>
+        </div>
+      ) : (
+        <div
+          className="sl-embed"
           style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
+            position: "relative",
+            paddingBottom: "calc(65.64% + 25px)",
             width: "100%",
-            height: "100%",
-            border: "1px solid rgba(63,95,172,0.35)",
-            boxShadow: "0px 0px 18px rgba(26, 19, 72, 0.15)",
-            borderRadius: "10px",
-            boxSizing: "border-box",
+            height: 0,
+            transform: "scale(1)",
           }}
-        ></iframe> */}
-      </div>
+        >
+          <iframe
+            loading="lazy"
+            class="sl-demo"
+            title="Storylane Embed"
+            src="https://app.storylane.io/demo/sj3fhbig4inz?embed=inline"
+            name="sl-embed"
+            allow="fullscreen"
+            allowfullscreen
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              border: "1px solid rgba(63,95,172,0.35)",
+              boxShadow: "0px 0px 18px rgba(26, 19, 72, 0.15)",
+              borderRadius: "10px",
+              boxSizing: "border-box",
+            }}
+          ></iframe>
+        </div>
+      )}
+        
     </div>
   );
 };
